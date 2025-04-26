130acce69cbc04270635da7fb81b4300f70dba20
Matvey <sinema3d00@gmail.com>
2025-04-26 22:34:32 +0300

    made looping back

---
+++ Assets/Game/Scripts/Walls/WallSpawner.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+
+namespace Game
+{
+    public class WallSpawner : MonoBehaviour
+    {
+        [SerializeField] private float _maxTime = 1.5f;
+        [SerializeField] private float _heightRange = 0.45f;
+        [SerializeField] private GameObject _pipe;
+        [SerializeField] float _destroyingTime = 10f;
+
+        private float _timer;
+
+
+        private void Start()
+        {
+            SpawnPipe();
+        }
+
+        private void Update()
+        {
+            if (_timer > _maxTime)
+            {
+                SpawnPipe();
+                _timer = 0;
+            }
+
+            _timer += Time.deltaTime;
+        }
+        private void SpawnPipe()
+        {
+            Vector3 spawnPos = transform.position + new Vector3(0, Random.Range(-_heightRange, _heightRange));
+            GameObject pipe = Instantiate(_pipe, spawnPos, Quaternion.identity);
+
+
+            Destroy(pipe, _destroyingTime);
+        }
+    }
+   
+}
